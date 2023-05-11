dd888825ba9126146c2b9ad18b34ff8875f0224f
Juani <quinterojuanignacio@gmail.com>
2023-05-11 09:40:46 +0200

    Update tateti.js

--- tateti.js
+++ tateti.js
@@ -1,6 +1,5 @@
 'use strict';
 
-var identificadores = [[document.getElementById("c1"),"c2","c3"],["c4","c5","c6"],["c7","c8","c9"]];
 
 function hover(identificador){
     let elem=document.getElementById(identificador);
@@ -13,14 +12,4 @@ function deshover(identificador){
 function dibujarElemento(identificador){
     let elem=document.getElementById(identificador);
     elem.innerHTML="O";
-}
-function basica(){
-    let gano=false;
-    for(let x=0;x<3;x++){
-        for(let y=0; y<3; y++){
-            if(identificadores[0][0]=="O"){
-                
-            }
-        }
-    }
 }
